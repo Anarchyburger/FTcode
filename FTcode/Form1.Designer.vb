ae1e7a2d9b55ce7d01767c5ec37de583ab540542
nschneider <nschneider@mhgi.net>
2015-12-16 14:18:04 -0500

    new1

---
+++ FTcode/Form1.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Checked = True
+        Me.RadioButton1.Location = New System.Drawing.Point(12, 11)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(72, 17)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Automatic"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(90, 11)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(60, 17)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.Text = "Manual"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 68.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(33, 48)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(349, 102)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "888888"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"AM", "PM"})
+        Me.ComboBox1.Location = New System.Drawing.Point(224, 10)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(45, 21)
+        Me.ComboBox1.TabIndex = 4
+        Me.ComboBox1.Text = "AM"
+        Me.ComboBox1.Visible = False
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"})
+        Me.ComboBox2.Location = New System.Drawing.Point(164, 10)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(54, 21)
+        Me.ComboBox2.TabIndex = 5
+        Me.ComboBox2.Text = "HOUR"
+        Me.ComboBox2.Visible = False
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"})
+        Me.ComboBox3.Location = New System.Drawing.Point(275, 10)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(52, 21)
+        Me.ComboBox3.TabIndex = 6
+        Me.ComboBox3.Text = "DAY"
+        Me.ComboBox3.Visible = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(342, 8)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(60, 23)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "Calculate"
+        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(414, 159)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Name = "Form1"
+        Me.Text = "Fast Track Access Code"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ FTcode/Form1.vb
@@ -0,0 +1,36 @@
+﻿
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim lookUp As Integer
+        Dim lookUp2 As Integer
+        Dim techCode() As Long = {8711, 7721, 6731, 5741, 4751, 3761, 2771, 1781, 0791, 9602, 8612, 7622, 6632, 5642, 4652, 3662,
+            2672, 1682, 0692, 9503, 8513, 7523, 6533, 5543, 4553, 3563, 2573, 1583, 0593, 9404, 8414, 7424, 6434, 5444, 4454, 3464, 2474, 1484, 0494, 9305,
+            8315, 7325, 6335, 5345, 4355, 3365, 2375, 1385, 0395, 9206, 8216, 7226, 6236, 5246, 4256}
+
+        Dim thisday As String = Microsoft.VisualBasic.DateAndTime.Day(Now)
+        Dim thisHour As String = Microsoft.VisualBasic.DateAndTime.Hour(Now)
+
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        'Label1.Visible = True
+        ComboBox1.Visible = False
+        ComboBox2.Visible = False
+        ComboBox3.Visible = False
+        Button1.Visible = False
+
+
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        'Label1.Visible = False
+        Label1.Text = "000000"
+        ComboBox1.Visible = True
+        ComboBox2.Visible = True
+        ComboBox3.Visible = True
+        Button1.Visible = True
+
+    End Sub
+End Class
